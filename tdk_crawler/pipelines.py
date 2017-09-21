87a4bb24ab55fac56bdbcd382210fe91effd6cf2
burak <tknbrk@gmail.com>
2017-09-21 06:38:08 +0200

    🌩 Fixed to output well-formed JSON file 🌩

--- tdk_crawler/pipelines.py
+++ tdk_crawler/pipelines.py
@@ -10,12 +10,16 @@ import codecs
 class TdkCrawlerPipeline(object):
 
 	def __init__(self):
-		self.file = codecs.open('data_encoded.json', 'a', encoding='utf-8')
+		self.file = codecs.open('data_encoded.json', 'w', encoding='utf-8')
 
 	def process_item(self, item, spider):
-		line = json.dumps(dict(item), sort_keys=True, indent=2, ensure_ascii=False)
-		self.file.write(line)
 		return item
 
-	def spider_closed(self, spider):
+	def close_spider(self, spider):
+		line = json.dumps(dict(spider.data), 
+			sort_keys=True, 
+			ensure_ascii=False, 
+			indent=4,
+			separators=(',',':'))
+		self.file.write(line)
 		self.file.close()
