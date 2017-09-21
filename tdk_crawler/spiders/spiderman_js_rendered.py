d88c0e24654174d0333a579da4b931d3513e10f3
burak <tknbrk@gmail.com>
2017-09-21 06:35:42 +0200

    found a parameter and disabled this version

---
+++ tdk_crawler/spiders/spiderman_js_rendered.py
@@ -0,0 +1,140 @@
+# # -*- coding: utf-8 -*-
+
+# # Project: TORNADO
+# # ---------------------------------------------------------
+# # Purpose: Crawling Turkish Language Society's web page
+# # (tdk.gov.tr) to get Turkish words and their meanings to 
+# # provide a better service to people who want to contribute
+# # improvement of Turkish language studies.
+# # ---------------------------------------------------------
+# __author__ = "Burak Tekin"
+# __copyright__ = "Copyleft 2017, Project Tornado"
+# __license__ = "MIT"
+# __version__ = "0.0.1"
+# __email__ = "tknbrk@gmail.com"
+# __status__ = "Staging"
+# __credits__ = [
+# 	{
+# 		'contributor': 'https://github.com/mertemin',
+# 		'contribution': 'Turkish word list'
+# 	}
+# ]
+
+# # RECIPE: 
+# # ---------------------------------------------------------
+# # listofwords(list) -> Spider() -> Output(JSON format file)
+# # Output Object: {word: {meaning: ["..."], meta_data:[...]}
+# # word: String, meaning: listofstring, meta_data: listofString
+# # ----------------------------------------------------------
+
+# import scrapy
+# import codecs
+# import json
+# import time
+# import sys
+# import os
+# import re
+
+# from definitions import ROOT_DIR
+# from scrapy.spiders import CrawlSpider
+# from scrapy.selector import HtmlXPathSelector
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from tdk_crawler.items import TdkCrawlerItem
+
+# # ----------------------------------------------------------
+# # Problem
+# # ----------------------------------------------------------
+# # No implicit word list exist on web page and no links exist
+# # to follow to crawl the page recursively. Query string 
+# # parameters are also hidden.
+# #
+# # Solution:
+# # ----------------------------------------------------------
+# # The problem above lead me to use webdriver to make request
+# # with words and crawl the result pages have been responded.
+# # ----------------------------------------------------------
+
+
+# # Code:
+# # ----------------------------------------------------------
+# browser = webdriver.Chrome(ROOT_DIR + "/chromedriver")
+# browser.implicitly_wait(30)
+# browser.get("http://www.tdk.gov.tr/index.php?option=com_gts&arama=gts")
+
+# class Spiderman(CrawlSpider):
+# 	name = 'tornado_js_rendered'
+
+# 	def create_word_list(self):
+# 		list_of_words = []
+# 		with codecs.open(ROOT_DIR + '/words/words.txt', 'r', encoding='utf-8') as words:
+# 			for word in words.readlines():
+# 				list_of_words.append(word)
+
+# 		return list_of_words
+
+# 	def __init__(self):
+# 		self.allowed_domains = ["www.tdk.gov.tr"]
+# 		self.start_urls = ["http://tdk.gov.tr/index.php?option=com_gts&arama=gts"]
+# 		self.dictionary = self.create_word_list()
+# 		self.limit = len(self.dictionary)
+
+# 	def word_log(self, word):
+# 		now = time.strftime("%c")
+# 		line = ""
+# 		with codecs.open(ROOT_DIR + '/words_missing.log', 'a+', encoding='utf-8') as log:
+# 			log.seek(0)
+# 			first_char = log.read()
+# 			if(first_char):
+# 				line = "<word>"+word + "</word>" + "\t" + now + "\n"
+# 			else:
+# 				line = "Missing word\tlog_saved_at\n"
+			
+# 			log.write(line)
+# 			log.flush()
+
+# 	def parse(self, response):
+# 		word_no = 0
+# 		data = dict()
+# 		data['data'] = dict()
+
+# 		while(word_no <= self.limit):
+# 			word = self.dictionary[word_no].replace('\n','')
+# 			print "\nCurrent Word: \t" + word + "\n" + "Word No: " + word_no + "\n"
+# 			try:
+# 				time.sleep(1) # time's given to let the DOM updates itself
+# 				input_field = browser.find_element_by_id('metin')
+# 				browser.execute_script("arguments[0].value = ''", input_field) # Update input field to request words
+# 				input_field.send_keys(word, Keys.RETURN)
+
+# 				# get source of the page returned after request
+# 				source = browser.page_source
+# 				hxs = scrapy.Selector(text=source)
+# 				meanings = hxs.xpath("//*[@id='hor-minimalist-a']/tbody/tr/td").extract()
+
+# 				for (index, meaning) in enumerate(meanings):
+# 					meaning = meaning.replace('\n\t\t', '').strip()
+# 					meaning_cleaned = re.findall(r'</i>(.*)<br>', meaning)
+# 					meta_data = re.findall(r'<i>(.*)</i>', meaning)
+					
+# 					tdk_item = TdkCrawlerItem()
+# 					tdk_item['meta'] = meta_data[0]
+# 					tdk_item['meaning'] = meaning_cleaned
+# 					tdk_item = dict(tdk_item)
+
+# 					if(word in data['data']):
+# 						data['data'][word].append(tdk_item)
+# 					else:
+# 						data['data'][word] = [tdk_item]
+# 			except:
+# 				print("Oops!",sys.exc_info()[0],"occured.")
+# 				self.word_log(word)
+# 				print("Next word...")
+
+# 			# Next word...
+# 			word_no += 1
+# 		yield data
+
+
+
+
