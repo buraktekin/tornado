63ef2ff98497e57f80e5f1de197739e77c7d2b04
buraktekin <tknbrk@gmail.com>
2019-08-09 00:07:16 +0200

    🎉🎊 Renovation 🎊🎉

---
+++ tornado.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Project: TORNADO
+# ---------------------------------------------------------
+# Purpose: Crawling Turkish Language Society's web page
+# (tdk.gov.tr) to get Turkish words and their meanings to 
+# provide a better service to people who want to contribute
+# improvement of Turkish language studies.
+# ---------------------------------------------------------
+__author__ = "Datallite"
+__copyright__ = "Copyleft 2019, Project Tornado"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__email__ = "datallite@web.de"
+__status__ = "Staging"
+__credits__ = [
+	{
+		'contributor': 'https://github.com/mertemin',
+		'contribution': 'Turkish word list'
+	}
+]
+
